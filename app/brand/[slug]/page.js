20a39f8bf91de555f73526625eb9ac06ad7f2961
pavlogembar12-droid <pavlogembar12@gmail.com>
2026-07-25 17:51:31 +0300

    Update page.js

--- app/brand/[slug]/page.js
+++ app/brand/[slug]/page.js
@@ -2,10 +2,7 @@ import { notFound } from 'next/navigation';
 import Link from 'next/link';
 import { getAllBrands, getProductsByBrand } from '@/lib/sheets';
 
-export async function generateStaticParams() {
-  const brands = await getAllBrands();
-  return brands.map((b) => ({ slug: b.slug }));
-}
+export const dynamic = 'force-dynamic';
 
 export async function generateMetadata({ params }) {
   const brands = await getAllBrands();
